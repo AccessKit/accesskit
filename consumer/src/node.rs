9898b979c54a899be99d237ed2280db92ed9f8b6
Matt Campbell <mattcampbell@pobox.com>
2021-07-14 16:16:07 -0500

    Add a few node helper functions

--- consumer/src/node.rs
+++ consumer/src/node.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use accesskit_schema::NodeId;
+use accesskit_schema::{NodeId, Role};
 use std::sync::{Arc, Weak};
 
 use crate::tree::{NodeState, Reader as TreeReader, Tree};
@@ -18,10 +18,30 @@ impl Node<'_> {
         &self.state.data
     }
 
+    pub fn is_focused(&self) -> bool {
+        return self.tree_reader.state.data.focus == Some(self.id())
+    }
+
+    pub fn is_ignored(&self) -> bool {
+        self.data().ignored || (self.role() == Role::Presentation)
+    }
+
+    pub fn is_invisible_or_ignored(&self) -> bool {
+        (self.is_invisible() || self.is_ignored()) && !self.is_focused()
+    }
+
     // Convenience getters
 
     pub fn id(&self) -> NodeId {
         self.data().id
+    }
+
+    pub fn role(&self) -> Role {
+        self.data().role
+    }
+
+    pub fn is_invisible(&self) -> bool {
+        self.data().invisible
     }
 }
 
