54ddcece16757f6a3c4c00223013a0fa1ef62a2b
Matt Campbell <mattcampbell@pobox.com>
2021-07-15 10:50:42 -0500

    First draft of Mac JNI library

---
+++ platforms/mac/jni/java/dev/accesskit/mac/AccessKitMacManager.java
@@ -0,0 +1,65 @@
+// Copyright 2021 The AccessKit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dev.accesskit.mac;
+
+public final class AccessKitMacManager implements AutoCloseable {
+    public static AccessKitMacManager forNSWindow(long nsWindow, String initialStateJson) {
+        long ptr = nativeNewForNSWindow(nsWindow, initialStateJson);
+        return new AccessKitMacManager(ptr);
+    }
+
+    public static AccessKitMacManager forNSView(long nsView, String initialStateJson) {
+        long ptr = nativeNewForNSView(nsView, initialStateJson);
+        return new AccessKitMacManager(ptr);
+    }
+
+    @Override
+    public void close() {
+        if (ptr != 0) {
+            nativeDestroy(ptr);
+            ptr = 0;
+        }
+    }
+
+    @Override
+    public void finalize() {
+        close();
+    }
+
+    public void update(String updateJson) {
+        checkActive();
+        nativeUpdate(ptr, updateJson);
+    }
+
+    /**
+     * Inject the accessibility tree into the specified window or view.
+     */
+    public void inject() {
+        checkActive();
+        nativeInject(ptr);
+    }
+
+    private long ptr;
+
+    private AccessKitMacManager(long ptr) {
+        this.ptr = ptr;
+    }
+
+    private void checkActive() {
+        if (ptr == 0) {
+            throw new IllegalStateException("already closed");
+        }
+    }
+
+    private static native long nativeNewForNSWindow(long nsWindow, String initialStateJson);
+    private static native long nativeNewForNSView(long nsView, String initialStateJson);
+    private static native void nativeDestroy(long ptr);
+    private static native void nativeUpdate(long ptr, String updateJson);
+    private static native void nativeInject(long ptr);
+
+    static {
+        System.loadLibrary("accesskit_mac_jni");
+    }
+}
---
+++ platforms/mac/jni/src/lib.rs
@@ -0,0 +1,84 @@
+// Copyright 2021 The AccessKit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use accesskit_mac::Manager;
+use accesskit_schema::TreeUpdate;
+use cocoa::appkit::NSWindow;
+use cocoa::base::id;
+use jni::JNIEnv;
+use jni::objects::{JClass, JString};
+use jni::sys::jlong;
+
+unsafe fn new_common(
+    env: JNIEnv,
+    view: id,
+    initial_state_json: JString,
+) -> jlong {
+    let initial_state_json: String = env
+        .get_string(initial_state_json)
+        .unwrap()
+        .into();
+    let initial_state = serde_json::from_str::<TreeUpdate>(&initial_state_json).unwrap();
+    let manager = Manager::new(view, initial_state);
+    Box::into_raw(Box::new(manager)) as jlong
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeNewForNSWindow(
+    env: JNIEnv,
+    _class: JClass,
+    window_ptr: jlong,
+    initial_state_json: JString,
+) -> jlong {
+    let window = window_ptr as id;
+    let view = window.contentView();
+    new_common(env, view, initial_state_json)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeNewForNSView(
+    env: JNIEnv,
+    _class: JClass,
+    view_ptr: jlong,
+    initial_state_json: JString,
+) -> jlong {
+    let view = view_ptr as id;
+    new_common(env, view, initial_state_json)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeDestroy(
+    _env: JNIEnv,
+    _class: JClass,
+    ptr: jlong,
+) {
+    let _boxed_manager = Box::from_raw(ptr as *mut Manager);
+    // Let the box drop at the end of the scope.
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeUpdate(
+    env: JNIEnv,
+    _class: JClass,
+    ptr: jlong,
+    update_json: JString,
+) {
+    let manager = &mut *(ptr as *mut Manager);
+    let update_json: String = env
+        .get_string(update_json)
+        .unwrap()
+        .into();
+    let update = serde_json::from_str::<TreeUpdate>(&update_json).unwrap();
+    manager.update(update);
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeInject(
+    _env: JNIEnv,
+    _class: JClass,
+    ptr: jlong,
+) {
+    let manager = &mut *(ptr as *mut Manager);
+    manager.inject();
+}
