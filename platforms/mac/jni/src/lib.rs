3231102f2b18f4e5afb74261b13a3b462ecc7c74
Matt Campbell <mattcampbell@pobox.com>
2021-07-18 07:15:30 -0500

    Tighten use of unsafe in Mac JNI library; eliminates more clippy warnings

--- platforms/mac/jni/src/lib.rs
+++ platforms/mac/jni/src/lib.rs
@@ -10,7 +10,7 @@ use jni::objects::{JClass, JString};
 use jni::sys::jlong;
 use jni::JNIEnv;
 
-unsafe fn new_common(env: JNIEnv, view: id, initial_state_json: JString) -> jlong {
+ fn new_common(env: JNIEnv, view: id, initial_state_json: JString) -> jlong {
     let initial_state_json: String = env.get_string(initial_state_json).unwrap().into();
     let initial_state = serde_json::from_str::<TreeUpdate>(&initial_state_json).unwrap();
     let manager = Manager::new(view, initial_state);
@@ -18,19 +18,19 @@ unsafe fn new_common(env: JNIEnv, view: id, initial_state_json: JString) -> jlon
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeNewForNSWindow(
+pub extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeNewForNSWindow(
     env: JNIEnv,
     _class: JClass,
     window_ptr: jlong,
     initial_state_json: JString,
 ) -> jlong {
     let window = window_ptr as id;
-    let view = window.contentView();
+    let view = unsafe { window.contentView() };
     new_common(env, view, initial_state_json)
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeNewForNSView(
+pub extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeNewForNSView(
     env: JNIEnv,
     _class: JClass,
     view_ptr: jlong,
@@ -41,34 +41,34 @@ pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeN
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeDestroy(
+pub extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeDestroy(
     _env: JNIEnv,
     _class: JClass,
     ptr: jlong,
 ) {
-    let _boxed_manager = Box::from_raw(ptr as *mut Manager);
+    let _boxed_manager = unsafe { Box::from_raw(ptr as *mut Manager) };
     // Let the box drop at the end of the scope.
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeUpdate(
+pub extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeUpdate(
     env: JNIEnv,
     _class: JClass,
     ptr: jlong,
     update_json: JString,
 ) {
-    let manager = &mut *(ptr as *mut Manager);
+    let manager = unsafe { &mut *(ptr as *mut Manager) };
     let update_json: String = env.get_string(update_json).unwrap().into();
     let update = serde_json::from_str::<TreeUpdate>(&update_json).unwrap();
     manager.update(update);
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeInject(
+pub extern "system" fn Java_dev_accesskit_mac_AccessKitMacManager_nativeInject(
     _env: JNIEnv,
     _class: JClass,
     ptr: jlong,
 ) {
-    let manager = &mut *(ptr as *mut Manager);
+    let manager = unsafe { &mut *(ptr as *mut Manager) };
     manager.inject();
 }
