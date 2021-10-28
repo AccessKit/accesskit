ae41ed480d2c8daae7cf69595715213299f85a4b
Matt Campbell <mattcampbell@pobox.com>
2021-10-28 16:13:18 -0500

    First non-trivial test

--- platforms/windows/src/tests/simple.rs
+++ platforms/windows/src/tests/simple.rs
@@ -3,7 +3,7 @@
 // the LICENSE-APACHE file) or the MIT license (found in
 // the LICENSE-MIT file), at your option.
 
-use std::num::NonZeroU64;
+use std::{convert::TryInto, num::NonZeroU64};
 
 use accesskit_schema::{Node, NodeId, Role, StringEncoding, Tree, TreeId, TreeUpdate};
 use windows::{runtime::*, Win32::UI::Accessibility::*};
@@ -57,6 +57,103 @@ fn has_native_uia() -> Result<()> {
     scope(WINDOW_TITLE, get_initial_state(), BUTTON_1_ID, |s| {
         let has_native_uia: bool = unsafe { UiaHasServerSideProvider(s.window) }.into();
         assert!(has_native_uia);
+        Ok(())
+    })
+}
+
+#[test]
+fn navigation() -> Result<()> {
+    scope(WINDOW_TITLE, get_initial_state(), BUTTON_1_ID, |s| {
+        let root = unsafe { s.uia.ElementFromHandle(s.window) }?;
+        let walker = unsafe { s.uia.RawViewWalker() }?;
+
+        // The children of the window include the children that we provide,
+        // but also the title bar provided by the OS. We know that our own
+        // children are in the order we specified, but we don't know
+        // their position relative to the title bar. In fact, Windows
+        // has changed this in the past.
+        //
+        // Note that a UIA client would normally use the UIA condition feature
+        // to traverse the tree looking for an element that meets
+        // some condition. But we want to be explicit about navigating
+        // forward (and backward below) through only the immediate children.
+        // We'll accept the performance hit of multiple cross-thread calls
+        // (insignificant in this case) to achieve that.
+        let mut button_1_forward: Option<IUIAutomationElement> = None;
+        let mut wrapped_child = unsafe { walker.GetFirstChildElement(&root) };
+        while let Ok(child) = wrapped_child {
+            let name = unsafe { child.CurrentName() }?;
+            let name: String = name.try_into().unwrap();
+            if name == "Button 1" {
+                button_1_forward = Some(child.clone());
+                break;
+            }
+            wrapped_child = unsafe { walker.GetNextSiblingElement(&child) };
+        }
+        let button_1_forward = button_1_forward.unwrap();
+        let control_type = unsafe { button_1_forward.CurrentControlType() }?;
+        assert_eq!(UIA_ButtonControlTypeId, control_type);
+
+        let mut button_2_forward: Option<IUIAutomationElement> = None;
+        let wrapped_child = unsafe { walker.GetNextSiblingElement(&button_1_forward) };
+        if let Ok(child) = wrapped_child {
+            let name = unsafe { child.CurrentName() }?;
+            let name: String = name.try_into().unwrap();
+            if name == "Button 2" {
+                button_2_forward = Some(child.clone());
+            }
+        }
+        let button_2_forward = button_2_forward.unwrap();
+        let control_type = unsafe { button_2_forward.CurrentControlType() }?;
+        assert_eq!(UIA_ButtonControlTypeId, control_type);
+
+        let mut button_2_backward: Option<IUIAutomationElement> = None;
+        let mut wrapped_child = unsafe { walker.GetLastChildElement(&root) };
+        while let Ok(child) = wrapped_child {
+            let name = unsafe { child.CurrentName() }?;
+            let name: String = name.try_into().unwrap();
+            if name == "Button 2" {
+                button_2_backward = Some(child.clone());
+                break;
+            }
+            wrapped_child = unsafe { walker.GetPreviousSiblingElement(&child) };
+        }
+        let button_2_backward = button_2_backward.unwrap();
+        let control_type = unsafe { button_2_backward.CurrentControlType() }?;
+        assert_eq!(UIA_ButtonControlTypeId, control_type);
+
+        let mut button_1_backward: Option<IUIAutomationElement> = None;
+        let wrapped_child = unsafe { walker.GetPreviousSiblingElement(&button_2_backward) };
+        if let Ok(child) = wrapped_child {
+            let name = unsafe { child.CurrentName() }?;
+            let name: String = name.try_into().unwrap();
+            if name == "Button 1" {
+                button_1_backward = Some(child.clone());
+            }
+        }
+        let button_1_backward = button_1_backward.unwrap();
+        let control_type = unsafe { button_1_backward.CurrentControlType() }?;
+        assert_eq!(UIA_ButtonControlTypeId, control_type);
+
+        let equal: bool =
+            unsafe { s.uia.CompareElements(&button_1_forward, &button_1_backward) }?.into();
+        assert!(equal);
+
+        let parent = unsafe { walker.GetParentElement(&button_1_forward) }?;
+        let equal: bool = unsafe { s.uia.CompareElements(&parent, &root) }?.into();
+        assert!(equal);
+
+        let desktop_root = unsafe { s.uia.GetRootElement() }?;
+        let parent = unsafe { walker.GetParentElement(&root) }?;
+        let equal: bool = unsafe { s.uia.CompareElements(&parent, &desktop_root) }?.into();
+        assert!(equal);
+
+        let wrapped_child = unsafe { walker.GetFirstChildElement(&button_1_forward) };
+        assert_eq!(Err(Error::OK), wrapped_child);
+
+        let wrapped_child = unsafe { walker.GetLastChildElement(&button_1_forward) };
+        assert_eq!(Err(Error::OK), wrapped_child);
+
         Ok(())
     })
 }
