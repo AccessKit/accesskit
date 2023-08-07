222616ab5e8182011df07d0326893baf08865102
Matt Campbell <mattcampbell@pobox.com>
2023-08-07 16:11:07 -0500

    Fix copyright year

--- platforms/macos/src/patch.rs
+++ platforms/macos/src/patch.rs
@@ -1,4 +1,4 @@
-// Copyright 2022 The AccessKit Authors. All rights reserved.
+// Copyright 2023 The AccessKit Authors. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (found in
 // the LICENSE-APACHE file) or the MIT license (found in
 // the LICENSE-MIT file), at your option.
