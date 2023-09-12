77f8e539de9f8257e1c445550d5e2efe3eafde63
Matt Campbell <mattcampbell@pobox.com>
2023-09-12 14:19:44 -0500

    stub implementation of TreeChangeHandler

--- platforms/web/src/adapter.rs
+++ platforms/web/src/adapter.rs
@@ -4,12 +4,12 @@
 // the LICENSE-MIT file), at your option.
 
 use accesskit::{ActionHandler, NodeId, TreeUpdate};
-use accesskit_consumer::Tree;
-use std::{cell::RefCell, collections::HashMap};
+use accesskit_consumer::{DetachedNode, Node, Tree, TreeChangeHandler, TreeState};
+use std::collections::HashMap;
 use web_sys::Element;
 
 pub struct Adapter {
-    tree: RefCell<Tree>,
+    tree: Tree,
     action_handler: Box<dyn ActionHandler>,
     root: Element,
     elements: HashMap<NodeId, Element>,
@@ -26,15 +26,44 @@ impl Adapter {
         let root = document.create_element("div").unwrap();
         parent.append_child(&root).unwrap();
         Self {
-            tree: RefCell::new(Tree::new(initial_state, true)),
+            tree: Tree::new(initial_state, true),
             action_handler,
             root,
             elements: HashMap::new(),
         }
     }
 
-    pub fn update(&self, update: TreeUpdate) {
-        // TODO: events
-        self.tree.borrow_mut().update(update);
+    pub fn update(&mut self, update: TreeUpdate) {
+        let mut handler = AdapterChangeHandler {
+            elements: &mut self.elements,
+        };
+        self.tree.update_and_process_changes(update, &mut handler);
+    }
+}
+
+struct AdapterChangeHandler<'a> {
+    elements: &'a mut HashMap<NodeId, Element>,
+}
+
+impl TreeChangeHandler for AdapterChangeHandler<'_> {
+    fn node_added(&mut self, node: &Node) {
+        // TODO
+    }
+
+    fn node_updated(&mut self, old_node: &DetachedNode, new_node: &Node) {
+        // TODO
+    }
+
+    fn focus_moved(
+        &mut self,
+        _old_node: Option<&DetachedNode>,
+        new_node: Option<&Node>,
+        _current_state: &TreeState,
+    ) {
+        // TODO
+    }
+
+    fn node_removed(&mut self, node: &DetachedNode, current_state: &TreeState) {
+        // TODO
     }
 }
