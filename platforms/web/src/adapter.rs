dac1c2b64f38e0280b846558636820ff9894b1b1
DataTriny <datatriny@gmail.com>
2023-09-13 12:45:42 +0200

    Create the root element and mapping between NodeId/Element

--- platforms/web/src/adapter.rs
+++ platforms/web/src/adapter.rs
@@ -3,13 +3,16 @@
 // the LICENSE-APACHE file) or the MIT license (found in
 // the LICENSE-MIT file), at your option.
 
-use accesskit::{ActionHandler, TreeUpdate};
+use accesskit::{ActionHandler, NodeId, TreeUpdate};
 use accesskit_consumer::Tree;
-use std::cell::RefCell;
+use std::{cell::RefCell, collections::HashMap};
+use web_sys::Element;
 
 pub struct Adapter {
     tree: RefCell<Tree>,
     action_handler: Box<dyn ActionHandler>,
+    root: Element,
+    elements: HashMap<NodeId, Element>,
 }
 
 impl Adapter {
@@ -18,9 +21,15 @@ impl Adapter {
         initial_state: TreeUpdate,
         action_handler: Box<dyn ActionHandler>,
     ) -> Self {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let parent = document.get_element_by_id(parent_id).unwrap();
+        let root = document.create_element("div").unwrap();
+        parent.append_child(&root).unwrap();
         Self {
             tree: RefCell::new(Tree::new(initial_state, true)),
             action_handler,
+            root,
+            elements: HashMap::new(),
         }
     }
 
