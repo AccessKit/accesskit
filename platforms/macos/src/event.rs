9565fe4a9df89017176293fce3df1cef0cfb3f4d
DataTriny <datatriny@gmail.com>
2024-01-03 18:40:33 +0100

    Remove set_object_for_key hack

--- platforms/macos/src/event.rs
+++ platforms/macos/src/event.rs
@@ -9,7 +9,7 @@ use icrate::{
     AppKit::*,
     Foundation::{NSMutableDictionary, NSNumber, NSString},
 };
-use objc2::{msg_send, runtime::AnyObject, Message};
+use objc2::runtime::{AnyObject, ProtocolObject};
 use std::{collections::HashSet, rc::Rc};
 
 use crate::{
@@ -17,15 +17,6 @@ use crate::{
     filters::{filter, filter_detached},
     node::NodeWrapper,
 };
-
-// Workaround for https://github.com/madsmtm/objc2/issues/306
-fn set_object_for_key<K: Message, V: Message>(
-    dictionary: &mut NSMutableDictionary<K, V>,
-    value: &V,
-    key: &K,
-) {
-    let _: () = unsafe { msg_send![dictionary, setObject: value, forKey: key] };
-}
 
 // This type is designed to be safe to create on a non-main thread
 // and send to the main thread. This ability isn't yet used though.
@@ -89,13 +80,19 @@ impl QueuedEvent {
 
                 let mut user_info = NSMutableDictionary::<_, AnyObject>::new();
                 let text = NSString::from_str(&text);
-                set_object_for_key(&mut user_info, &*text, unsafe {
-                    NSAccessibilityAnnouncementKey
-                });
+                unsafe {
+                    user_info.setObject_forKey(
+                        &*text,
+                        ProtocolObject::from_ref(NSAccessibilityAnnouncementKey),
+                    )
+                };
                 let priority = NSNumber::new_isize(priority);
-                set_object_for_key(&mut user_info, &*priority, unsafe {
-                    NSAccessibilityPriorityKey
-                });
+                unsafe {
+                    user_info.setObject_forKey(
+                        &*priority,
+                        ProtocolObject::from_ref(NSAccessibilityPriorityKey),
+                    )
+                };
 
                 unsafe {
                     NSAccessibilityPostNotificationWithUserInfo(
