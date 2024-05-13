411f222e051905bb92f60dcd6b6958b46cd4e769
Matt Campbell <mattcampbell@pobox.com>
2024-05-13 15:24:17 -0500

    fix: Fix platform adapters to support copy-on-write tree snapshots again

--- platforms/macos/src/filters.rs
+++ platforms/macos/src/filters.rs
@@ -3,6 +3,4 @@
 // the LICENSE-APACHE file) or the MIT license (found in
 // the LICENSE-MIT file), at your option.
 
-pub(crate) use accesskit_consumer::{
-    common_filter as filter, common_filter_detached as filter_detached,
-};
+pub(crate) use accesskit_consumer::common_filter as filter;
