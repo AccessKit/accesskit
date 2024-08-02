a7f0153f5be6a412706ee7d9360920af5016d1af
Matt Campbell <mattcampbell@pobox.com>
2024-08-02 08:19:03 -0500

    Base native support for touch exploration

--- platforms/android/src/util.rs
+++ platforms/android/src/util.rs
@@ -4,11 +4,14 @@
 // the LICENSE-MIT file), at your option.
 
 use accesskit::NodeId;
+use accesskit_consumer::Node;
 use jni::{
     objects::JObject,
     sys::{jint, jvalue},
 };
 use std::collections::HashMap;
+
+pub(crate) const HOST_VIEW_ID: jint = -1;
 
 #[derive(Default)]
 pub(crate) struct NodeIdMap {
@@ -22,7 +25,11 @@ impl NodeIdMap {
         self.java_to_accesskit.get(&java_id).copied()
     }
 
-    pub(crate) fn get_or_create_java_id(&mut self, accesskit_id: NodeId) -> jint {
+    pub(crate) fn get_or_create_java_id(&mut self, node: &Node) -> jint {
+        if node.is_root() {
+            return HOST_VIEW_ID;
+        }
+        let accesskit_id = node.id();
         if let Some(id) = self.accesskit_to_java.get(&accesskit_id) {
             return *id;
         }
@@ -38,9 +45,9 @@ pub(crate) fn bool_value(value: bool) -> jvalue {
     jvalue { z: value as u8 }
 }
 
-pub(crate) fn id_value(id_map: &mut NodeIdMap, value: NodeId) -> jvalue {
+pub(crate) fn id_value(id_map: &mut NodeIdMap, node: &Node) -> jvalue {
     jvalue {
-        i: id_map.get_or_create_java_id(value),
+        i: id_map.get_or_create_java_id(node),
     }
 }
 
