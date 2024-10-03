27eb6b70fe86a5dd6fae1c6a6e4e2b67fa575247
Alain Emilia Anna Zscheile <fogti+devel@ytrizja.de>
2024-10-03 16:51:14 +0200

    fix(atspi): store bus_name as String

--- platforms/unix/src/atspi/object_address.rs
+++ platforms/unix/src/atspi/object_address.rs
@@ -6,8 +6,8 @@
 use atspi::ObjectRef;
 use serde::{Deserialize, Serialize};
 use zbus::{
-    names::{OwnedUniqueName, UniqueName},
-    zvariant::{ObjectPath, OwnedObjectPath, OwnedValue, Type, Value},
+    names::UniqueName,
+    zvariant::{ObjectPath, OwnedObjectPath, OwnedValue, Str, Type, Value},
 };
 
 // https://gnome.pages.gitlab.gnome.org/at-spi2-core/libatspi/const.DBUS_PATH_NULL.html
@@ -15,21 +15,21 @@ const NULL_PATH: &str = "/org/a11y/atspi/null";
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, OwnedValue, Type, Value)]
 pub(crate) struct OwnedObjectAddress {
-    bus_name: OwnedUniqueName,
+    bus_name: String,
     path: OwnedObjectPath,
 }
 
 impl OwnedObjectAddress {
     pub(crate) fn new(bus_name: &UniqueName, path: OwnedObjectPath) -> Self {
         Self {
-            bus_name: bus_name.to_owned().into(),
+            bus_name: bus_name.to_string(),
             path,
         }
     }
 
     pub(crate) fn null() -> Self {
         Self {
-            bus_name: UniqueName::from_static_str(":0.0").unwrap().into(),
+            bus_name: String::new(),
             path: ObjectPath::from_str_unchecked(NULL_PATH).into(),
         }
     }
@@ -38,7 +38,7 @@ impl OwnedObjectAddress {
 impl From<ObjectRef> for OwnedObjectAddress {
     fn from(object: ObjectRef) -> Self {
         Self {
-            bus_name: object.name,
+            bus_name: Str::from(object.name).into(),
             path: object.path,
         }
     }
