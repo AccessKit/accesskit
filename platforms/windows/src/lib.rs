7487b0f9faa81ca5a764e213d12a4b4067d0bd8d
Matt Campbell <mattcampbell@pobox.com>
2024-12-17 21:56:35 -0600

    Re-export the new accesskit_consumer types we now publicly refer to in the Windows crate

--- platforms/windows/src/lib.rs
+++ platforms/windows/src/lib.rs
@@ -16,7 +16,7 @@ pub use adapter::{Adapter, QueuedEvents};
 mod subclass;
 pub use subclass::SubclassingAdapter;
 
-pub use accesskit_consumer::TreeUpdate;
+pub use accesskit_consumer::{BoxedActivationHandler, NonGenericActivationHandler, TreeUpdate};
 pub use windows::Win32::Foundation::{HWND, LPARAM, LRESULT, WPARAM};
 
 #[cfg(test)]
