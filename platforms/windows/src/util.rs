e0e2e478b34c498393f2d1cbf27db3c9231b9629
Matt Campbell <mattcampbell@pobox.com>
2024-12-22 09:45:59 -0600

    Reuse the string buffer in window_title. This gets us down to 0 allocations in steady state in the Windows hello_world example.

--- platforms/windows/src/util.rs
+++ platforms/windows/src/util.rs
@@ -242,7 +242,7 @@ pub(crate) fn client_top_left(hwnd: WindowHandle) -> Point {
     Point::new(result.x.into(), result.y.into())
 }
 
-pub(crate) fn window_title(hwnd: WindowHandle) -> Option<BSTR> {
+pub(crate) fn window_title(hwnd: WindowHandle, buffer: &mut Vec<u16>) -> Option<BSTR> {
     // The following is an old hack to get the window caption without ever
     // sending messages to the window itself, even if the window is in
     // the same process but possibly a separate thread. This prevents
@@ -253,7 +253,8 @@ pub(crate) fn window_title(hwnd: WindowHandle) -> Option<BSTR> {
         return None;
     }
     let capacity = (result.0 as usize) + 1; // make room for the null
-    let mut buffer = Vec::<u16>::with_capacity(capacity);
+    buffer.clear();
+    buffer.reserve(capacity);
     let result = unsafe {
         DefWindowProcW(
             hwnd.0,
@@ -267,7 +268,7 @@ pub(crate) fn window_title(hwnd: WindowHandle) -> Option<BSTR> {
     }
     let len = result.0 as usize;
     unsafe { buffer.set_len(len) };
-    Some(BSTR::from_wide(&buffer).unwrap())
+    Some(BSTR::from_wide(buffer).unwrap())
 }
 
 pub(crate) fn toolkit_description<'a>(
