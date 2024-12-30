de9a11e20e0727d6bc883d26cf105e420bb477da
Matt Campbell <mattcampbell@pobox.com>
2024-12-30 13:28:51 -0600

    fmt

--- platforms/winit/src/platform_impl/android.rs
+++ platforms/winit/src/platform_impl/android.rs
@@ -49,7 +49,8 @@ impl Adapter {
     pub fn process_event(&mut self, _window: &Window, event: &WindowEvent) {
         match event {
             WindowEvent::CursorMoved { position, .. } => {
-                self.adapter.handle_hover_enter_or_move(position.x as _, position.y as _);
+                self.adapter
+                    .handle_hover_enter_or_move(position.x as _, position.y as _);
             }
             WindowEvent::CursorLeft { .. } => {
                 self.adapter.handle_hover_exit();
