a5a9cfeab43f57dff175dbe8728f1825c63bc536
Matt Campbell <mattcampbell@pobox.com>
2025-02-24 06:57:30 -0600

    Try to add a CI job for verifying the committed .dex file

--- platforms/android/build-dex.sh
+++ platforms/android/build-dex.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
+set -e -u -o pipefail
+cd `dirname $0`
 ANDROID_JAR=$ANDROID_HOME/platforms/android-30/android.jar
 find java -name '*.class' -delete
 javac --source 8 --target 8 --boot-class-path $ANDROID_JAR -Xlint:deprecation `find java -name '*.java'`
