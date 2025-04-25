0316518b94cf1bc9755e67f0cf48e37c096975fa
Matt Campbell <mattcampbell@pobox.com>
2025-04-25 05:56:12 -0500

    refactor!: Use the queued-events pattern in the Android adapter (#555)

--- platforms/android/src/util.rs
+++ platforms/android/src/util.rs
@@ -13,6 +13,7 @@ pub(crate) const ACTION_CLICK: jint = 1 << 4;
 pub(crate) const ACTION_NEXT_AT_MOVEMENT_GRANULARITY: jint = 1 << 8;
 pub(crate) const ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY: jint = 1 << 9;
 pub(crate) const ACTION_SET_SELECTION: jint = 1 << 17;
+pub(crate) const EVENT_VIEW_CLICKED: jint = 1;
 pub(crate) const EVENT_VIEW_FOCUSED: jint = 1 << 3;
 pub(crate) const EVENT_WINDOW_CONTENT_CHANGED: jint = 1 << 11;
 pub(crate) const HOST_VIEW_ID: jint = -1;
