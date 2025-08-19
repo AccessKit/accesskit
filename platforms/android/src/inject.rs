c88e4867f7f9a2dd40ab4b15aad4e83307116aca
Arnold Loubriat <datatriny@gmail.com>
2025-08-19 19:28:10 +0200

    Don't mention prebuilt in the documentation anymore

--- platforms/android/src/inject.rs
+++ platforms/android/src/inject.rs
@@ -295,7 +295,7 @@ fn delegate_class(env: &mut JNIEnv) -> &'static JClass<'static> {
 ///
 /// This depends on the Java `dev.accesskit.android.Delegate` class, the source
 /// code for which is in this crate's `java` directory. If the `embedded-dex`
-/// feature is enabled, then that class is loaded from a prebuilt `.dex` file
+/// feature is enabled, then that class is loaded from a `.dex` file
 /// that this crate embeds. Otherwise, it's simply assumed that the class
 /// is in the application package. None of this type's public functions
 /// make assumptions about whether they're called from the Android UI thread.
