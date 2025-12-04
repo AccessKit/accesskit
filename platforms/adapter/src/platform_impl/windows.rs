cac54b5be889023f17ad111300e04eff140593f9
Nico Burns <nico@nicoburns.com>
2025-12-04 01:02:51 +0000

    Add accesskit_adapter crate. Upgrade accesskit_winit to winit 0.31.0-beta.2

--- platforms/adapter/src/platform_impl/windows.rs
+++ platforms/adapter/src/platform_impl/windows.rs
@@ -1,13 +1,10 @@
 // Copyright 2022 The AccessKit Authors. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (found in
 // the LICENSE-APACHE file).
-#[cfg(feature = "rwh_05")]
-use crate::raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
-#[cfg(feature = "rwh_06")]
-use crate::raw_window_handle::{HasWindowHandle, RawWindowHandle};
 
 use accesskit::{ActionHandler, ActivationHandler, DeactivationHandler, TreeUpdate};
 use accesskit_windows::{SubclassingAdapter, HWND};
+use raw_window_handle::RawWindowHandle;
 use winit::{event::WindowEvent, event_loop::ActiveEventLoop, window::Window};
 
 pub struct Adapter {
@@ -16,20 +13,12 @@ pub struct Adapter {
 
 impl Adapter {
     pub fn new(
-        _event_loop: &ActiveEventLoop,
-        window: &Window,
+        window_handle: &RawWindowHandle,
         activation_handler: impl 'static + ActivationHandler,
         action_handler: impl 'static + ActionHandler + Send,
         _deactivation_handler: impl 'static + DeactivationHandler,
     ) -> Self {
-        #[cfg(feature = "rwh_05")]
-        let hwnd = match window.raw_window_handle() {
-            RawWindowHandle::Win32(handle) => handle.hwnd,
-            RawWindowHandle::WinRt(_) => unimplemented!(),
-            _ => unreachable!(),
-        };
-        #[cfg(feature = "rwh_06")]
-        let hwnd = match window.window_handle().unwrap().as_raw() {
+        let hwnd = match window_handle {
             RawWindowHandle::Win32(handle) => handle.hwnd.get() as *mut _,
             RawWindowHandle::WinRt(_) => unimplemented!(),
             _ => unreachable!(),
@@ -45,5 +34,7 @@ impl Adapter {
         }
     }
 
-    pub fn process_event(&mut self, _window: &Window, _event: &WindowEvent) {}
+    pub fn set_focus(&mut self, is_focused: bool) {}
+
+    pub fn set_window_bounds(&mut self, outer_bounds: Rect, inner_bounds: Rect) {}
 }
